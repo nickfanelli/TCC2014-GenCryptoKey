8564a48984a91af31a06e193ecd8cd55664aec9d
Nicholas Fanelli Hugueney <nicholashugueney@gmail.com>
2014-06-08 20:23:09 -0300

    Enhanced settings window & database tables creation

--- GenCryptoKey/src/br/pucc/engComp/GenCryptoKey/controller/DB.java
+++ GenCryptoKey/src/br/pucc/engComp/GenCryptoKey/controller/DB.java
@@ -1,15 +1,20 @@
 package br.pucc.engComp.GenCryptoKey.controller;
 
 import java.sql.Connection;
+import java.sql.DatabaseMetaData;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
 
 public class DB {
-	
+
 	private static Connection con = null;
-	
+
 	public static Connection getConnection() {
-		if(con == null) {
-			try{
+		if (con == null) {
+			try {
 				Class.forName("org.apache.derby.jdbc.EmbeddedDriver").newInstance();
 				con = DriverManager.getConnection("jdbc:derby:cryptokey;create=true;user=nicholas;pass=CryptoAnita");
 			} catch (Exception e) {
@@ -18,5 +23,62 @@ public class DB {
 			}
 		}
 		return con;
+	}
+
+	protected static class Pair<F, S> {
+		private F first;
+		private S second;
+
+		/* Getters & Setters */
+		public F getFirst() {
+			return first;
+		}
+
+		public void setFirst(F first) {
+			this.first = first;
+		}
+
+		public S getSecond() {
+			return second;
+		}
+
+		public void setSecond(S second) {
+			this.second = second;
+		}
+	}
+
+	static {
+		getConnection();
+		List<Pair<String, String>> tables = new ArrayList<Pair<String, String>>();
+		Pair<String, String> userInfoTable = new Pair<String, String>();
+		userInfoTable.setFirst("USERINFO");
+		userInfoTable.setSecond("create table USERINFO (ID int not null generated always as identity(start with 1, increment by 1), NOME varchar(50), SOBRENOME varchar(50), EMAIL varchar(100), USERNAME varchar(20), PASSWORD varchar(30), primary key(ID))");
+		
+		Pair<String, String> settingsTable = new Pair<String, String>();
+		settingsTable.setFirst("GASETTINGS");
+		settingsTable.setSecond("create table GASETTINGS (ID int not null generated always as identity(start with 1, increment by 1), INDIVIDUALSIZE int, POPULATIONSIZE int, CROSSOVERPOINTS int, MUTATIONRATE int, PRESERVEDINDIVIDUALS int, GENERATIONSTOSTOP int, primary key(ID))");
+		
+		Pair<String, String> generatedKeysTable = new Pair<String, String>();
+		generatedKeysTable.setFirst("GENERATEDKEYS");
+		generatedKeysTable.setSecond("create table GENERATEDKEYS (ID int not null generated always as identity(start with 1, increment by 1), GENERATEDKEY varchar(512), GENERATIONTIMESTAMP timestamp, primary key(ID))");
+		
+		tables.add(userInfoTable);
+		tables.add(settingsTable);
+		tables.add(generatedKeysTable); 
+		
+		try {
+			DatabaseMetaData dbmd = con.getMetaData();
+			for (Pair<String, String> table : tables) {
+				ResultSet rs = dbmd.getTables(null, null, table.getFirst(), null);
+				if (!rs.next()) {
+					PreparedStatement ps = con.prepareStatement(table.getSecond());
+					if (ps.executeUpdate() == -1) {
+						throw new Exception("Falha ao criar tabela " + table.getFirst());
+					}
+				}
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 }
--- GenCryptoKey/src/br/pucc/engComp/GenCryptoKey/views/GASettings.java
+++ GenCryptoKey/src/br/pucc/engComp/GenCryptoKey/views/GASettings.java
@@ -6,6 +6,7 @@ import java.awt.GridLayout;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JLabel;
+import javax.swing.JSlider;
 import javax.swing.JTextField;
 import javax.swing.JCheckBox;
 import javax.swing.JButton;
@@ -26,66 +27,119 @@ public class GASettings extends JFrame {
 		final JFrame settingsFrame = new JFrame("Genetic Algorithm Settings");
     	
     	String[] settingsLabels = {"Individual size: ", "Population size: ", "Crossover points: ", "Mutation rate: ",
-    						"Preserved individuals: ", "Fit individuals to stop: ", "Generations to stop: "};
-    	String[] defaultSettingsValues = {"192", "500", "2", "0.015", "50", "1", "2000"};
+    								"Preserved individuals: ", "Fit individuals to stop: ", "Generations to stop: "};
+    	String[] defaultSettingsValues = {"192", "500", "1", "0.015", "50", "1", "2000"};
+    	String[] minimumParameterValues = {"128", "50", "1", "0.005", "10", "0", "500"};
+    	String[] maximumParameterValues = {"512", "5000", "10", "0.03", "500", "5", "10000"};    	
     	
     	int numLabels = settingsLabels.length;
     	
-    	ArrayList<JTextField> valueFields = new ArrayList<JTextField>();
+    	ArrayList<JSlider> settingsSliders = new ArrayList<JSlider>();
     	JPanel parentPanel = new JPanel(new BorderLayout());
     	
+    	/*
     	JPanel headerPanel = new JPanel(new SpringLayout());
     	headerPanel.setOpaque(true);
     	JLabel invisLabel = new JLabel();
     	JTextField invisTextField = new JTextField("5");
     	JLabel defaultValuesLabel = new JLabel("Default");
+    	JLabel minimumValuesLabel = new JLabel("Min");
+    	JLabel maximumValuesLabel = new JLabel("Max");
     	invisLabel.setVisible(false);
     	invisTextField.setVisible(false);
     	headerPanel.add(invisLabel);
 		headerPanel.add(invisTextField);
 		headerPanel.add(defaultValuesLabel);
+		headerPanel.add(minimumValuesLabel);
+		headerPanel.add(maximumValuesLabel);
     	
     	// Formata o painel header e cria o grid
     	SpringUtilities.makeCompactGrid(headerPanel,
-    	                                (1), 3, // linhas, colunas
+    	                                1, 5, // linhas, colunas
     	                                6, 6,        // initX, initY
     	                                6, 6);       // xPad, yPad
+    	*/
     	
     	// Cria e popula o painel para o grid
     	JPanel springPanel = new JPanel(new SpringLayout());
     	springPanel.setOpaque(true);
     	
     	for (int i = 0; i < numLabels; i++) {
-    	    JLabel l = new JLabel(settingsLabels[i], JLabel.TRAILING);
-    	    springPanel.add(l);
+    		// Creating settings components ...
+    		// Parameter label
+    	    JLabel parameterName = new JLabel(settingsLabels[i], JLabel.TRAILING);
+    	    springPanel.add(parameterName);
+    	    // Parameter slider
+    	    if(i == 3){
+    	    	JTextField newValueTextField = new JTextField(defaultSettingsValues[i]);
+    	    	newValueTextField.setEnabled(true);
+        	    springPanel.add(newValueTextField);
+        	    //valueFields.add(newValueTextField);
+    	    }else{
+    	    	JSlider parameterSlider = new JSlider(Integer.parseInt(minimumParameterValues[i]), 
+						Integer.parseInt(maximumParameterValues[i]), 
+						Integer.parseInt(defaultSettingsValues[i]));
+				parameterName.setLabelFor(parameterSlider);
+				springPanel.add(parameterSlider);
+				settingsSliders.add(parameterSlider);
+    	    }
+    	    
+    	    
+    	    
+    	    JButton resetParamButton = new JButton("Reset");
+    	    springPanel.add(resetParamButton);
+    	    
+    	    /*
+    	    // Parameter (current) value
     	    JTextField newValueTextField = new JTextField(5);
     	    newValueTextField.setText(defaultSettingsValues[i]);
     	    l.setLabelFor(newValueTextField);
     	    springPanel.add(newValueTextField);
     	    valueFields.add(newValueTextField);
+    	    // Parameter default value
     	    JTextField defaultValueTextField = new JTextField(defaultSettingsValues[i]);
-    	    defaultValueTextField.setEditable(false);
+    	    defaultValueTextField.setEnabled(false);
     	    springPanel.add(defaultValueTextField);
+    	    // Parameter minimum values
+    	    JTextField minimumValueTextField = new JTextField(minimumParameterValues[i]);
+    	    minimumValueTextField.setEnabled(false);
+    	    springPanel.add(minimumValueTextField);
+    	    // Parameter maximum values
+    	    JTextField maximumValueTextField = new JTextField(maximumParameterValues[i]);
+    	    maximumValueTextField.setEnabled(false);
+    	    springPanel.add(maximumValueTextField);
+    	    */
     	}    	
     	
     	JLabel scheduleLabel = new JLabel("Schedule key generation: ");
     	JCheckBox scheduleCheckBox = new JCheckBox();
     	JTextField scheduleTextField = new JTextField("No");
+    	//JTextField invisLabel2 = new JTextField("5");
+    	//JTextField invisLabel3 = new JTextField("5");
     	scheduleTextField.setEnabled(false);
+    	//invisLabel2.setVisible(false);
+    	//invisLabel3.setVisible(false);
     	
     	springPanel.add(scheduleLabel);
     	springPanel.add(scheduleCheckBox);
     	springPanel.add(scheduleTextField);
+    	//springPanel.add(invisLabel2);
+    	//springPanel.add(invisLabel3);
     	
     	JLabel writeLogLabel = new JLabel("Enable execution log: ");
     	JCheckBox writeLogCheckBox = new JCheckBox();
     	JTextField writeLogTextField = new JTextField("No");
+    	//JTextField invisLabel4 = new JTextField("5");
+    	//JTextField invisLabel5 = new JTextField("5");
     	writeLogTextField.setEnabled(false);
+    	//invisLabel4.setVisible(false);
+    	//invisLabel5.setVisible(false);
     	
     	springPanel.add(writeLogLabel);
     	springPanel.add(writeLogCheckBox);
     	springPanel.add(writeLogTextField);
-    	
+    	//springPanel.add(invisLabel4);
+    	//springPanel.add(invisLabel5);
     	// Formata o painel e cria o grid
     	SpringUtilities.makeCompactGrid(springPanel,
     	                                (numLabels + 2), 3, // linhas, colunas
@@ -100,6 +154,7 @@ public class GASettings extends JFrame {
 			@Override
     		public void actionPerformed(ActionEvent e) {
     			// TODO Save user values to database
+				// Verify that all fields are numbers and aren't empty
             }
     	});
     	JButton cancelButton = new JButton("Cancel");
@@ -123,7 +178,7 @@ public class GASettings extends JFrame {
     	buttonsPanel.add(cancelButton);
     	buttonsPanel.add(resetButton);
     	
-    	parentPanel.add(headerPanel, BorderLayout.NORTH);
+    	//parentPanel.add(headerPanel, BorderLayout.NORTH);
     	parentPanel.add(springPanel, BorderLayout.CENTER);
     	parentPanel.add(buttonsPanel, BorderLayout.SOUTH);
     	settingsFrame.setContentPane(parentPanel);
